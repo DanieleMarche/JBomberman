e49ea26eba9281c8b68798b0a294127385ef85d9
Daniele Marchetilli <marchetilli@gmail.com>
2023-09-29 23:27:28 +0200

    centered elements on screen

--- src/main/MenuPages/LostPanel.java
+++ src/main/MenuPages/LostPanel.java
@@ -1,5 +1,4 @@
 package main.MenuPages;
-
 import User.UserModel;
 import main.GameFrame;
 
@@ -14,12 +13,10 @@ public class LostPanel extends JPanel {
     private JButton mainMenuButton;
 
     public LostPanel(CardLayout cardLayout, JFrame parentFrame, JPanel parentPanel, UserModel usermodel) {
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout()); // Use GridBagLayout for centering
 
         titleLabel = new JLabel("Hai perso");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(titleLabel, BorderLayout.NORTH);
 
         restartButton = new JButton("Ricomincia livello " + usermodel.getLivelloRaggiunto().getLevelCode() + ".");
         mainMenuButton = new JButton("Torna al menu principale");
@@ -27,7 +24,22 @@ public class LostPanel extends JPanel {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         buttonPanel.add(restartButton);
         buttonPanel.add(mainMenuButton);
-        add(buttonPanel, BorderLayout.CENTER);
+
+        // Create a GridBagConstraints to center components
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0; // Make components expand horizontally
+        gbc.insets = new Insets(10, 10, 10, 10); // Add some padding
+
+        // Add titleLabel at the top
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        add(titleLabel, gbc);
+
+        // Add buttonPanel in the center
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(buttonPanel, gbc);
 
         restartButton.addActionListener(new ActionListener() {
             @Override
@@ -45,4 +57,5 @@ public class LostPanel extends JPanel {
         });
     }
 }
+
 
